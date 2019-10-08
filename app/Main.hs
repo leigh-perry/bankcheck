7cca23f457d9e742d47d2e84d8a02753fb037337
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-08 22:37:32 +1100

    Initial CSV parsing

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,9 @@
+import           Analyser
 import           Data.Semigroup      ((<>))
 import           Options.Applicative
+
+main :: IO ()
+main = run =<< execParser (parseOptions ? "Bank check operations")
 
 type GlobalOption = String
 
@@ -13,20 +17,18 @@ data Command
 -- bankcheck --global-option analyse filename ... filename
 -- bankcheck --global-option dummy <build-id>
 data Options =
-  Options GlobalOption Command
-
-main :: IO ()
-main = run =<< execParser (parseOptions ? "Bank check operations")
+  Options (Maybe GlobalOption) Command
 
 run :: Options -> IO ()
 run (Options globalOption cmd) =
   case cmd of
-    Analyse filePath -> putStrLn $ globalOption <> " : " <> show filePath
-    Dummy buildId    -> putStrLn $ globalOption <> " : " <> buildId
+    Analyse filepaths -> analyse filepaths
+    Dummy buildId     -> putStrLn $ show globalOption <> " : " <> buildId
 
 parseOptions :: Parser Options
 parseOptions =
-  Options <$> strOption (short 'g' <> long "global-option" <> metavar "GLOBALOPTION" <> help "Some option global to all commands") <*>
+  Options <$>
+  optional (strOption (short 'g' <> long "global-option" <> metavar "GLOBALOPTION" <> help "Some option global to all commands")) <*>
   subparser
     (command "analyse" ((Analyse <$> some (argument str (metavar "SOURCE-FILEPATH"))) ? "Analyse statements in specified files") <>
      command "dummy" ((Dummy <$> argument str (metavar "BUILD-ID")) ? "Dummy command taking a build id"))
--- src/Analyser.hs
+++ src/Analyser.hs
@@ -1,6 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Analyser
-  ( run
+  ( analyse
   ) where
+
+import qualified Data.ByteString.Lazy as BL
+import           Data.Csv             ((.:))
+import qualified Data.Csv             as Csv
+import           Data.Foldable        (traverse_)
+import           Data.Functor         (void)
+import           Data.Text
+import qualified Data.Vector          as V
 
 --import Debug.Trace
 {-
@@ -13,5 +23,38 @@ module Analyser
     . compare with whitelist
     . show remaining groups
 -}
-run :: [FilePath] -> IO ()
-run = print
+analyse :: [FilePath] -> IO ()
+analyse filepaths =
+  void $ do
+    files <- traverse analyseFile filepaths -- IO [V.Vector Entry]
+    traverse dump files -- IO [()]
+
+dump :: V.Vector Entry -> IO ()
+dump v = traverse_ print v
+
+----
+-- Effective Date,Entered Date,Transaction Description,Amount,Balance
+-- ,20190701,"VISA-TRANSPORTFORNSW OPAL     CHIPPENDALE  AU#0428976(Ref.070100303466)",-20.00,-11846.85
+data Entry =
+  Entry
+    { eEffectiveDate :: !Text
+    , eEnteredDate   :: !Text
+    , eDescription   :: !Text
+    , eAmount        :: !Double
+    , eBalance       :: !Double
+    }
+  deriving (Show, Eq)
+
+instance Csv.FromNamedRecord Entry where
+  parseNamedRecord r =
+    Entry <$> r .: "Effective Date" <*> r .: "Entered Date" <*> r .: "Transaction Description" <*> r .: "Amount" <*>
+    r .: "Balance"
+
+analyseFile :: String -> IO (V.Vector Entry)
+analyseFile filepath = do
+  csvData <- BL.readFile filepath
+  let d = Csv.decodeByName csvData :: Either String (Csv.Header, V.Vector Entry)
+  return $
+    case d of
+      Left err     -> error err -- TODO ExceptT
+      Right (_, v) -> v
