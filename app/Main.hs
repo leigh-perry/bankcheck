066f7cf570bf9bb5ac697826e22e31bf3546ea71
s117476 <Leigh.Perry@iag.com.au>
2019-10-10 14:53:44 +1100

    Basic filtering

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
-import qualified Analyser
-import Control.Monad.Trans.Except
-import Data.Functor (void)
-import Data.Semigroup ((<>))
-import Options.Applicative
+import qualified Analyser                   as A
+import           Control.Monad.Trans.Except
+import           Data.Functor               (void)
+import           Data.Semigroup             ((<>))
+import           Options.Applicative
 
 main :: IO ()
 main = run =<< execParser (parseOptions ? "Bank check operations")
@@ -12,7 +12,7 @@ type GlobalOption = String
 type BuildId = String
 
 data Command
-  = Analyse [FilePath]
+  = Analyse [FilePath] (Maybe Integer) (Maybe Integer)
   | Dummy BuildId
 
 -- approach from https://thoughtbot.com/blog/applicative-options-parsing-in-haskell
@@ -24,19 +24,25 @@ data Options =
 run :: Options -> IO ()
 run (Options globalOption cmd) =
   case cmd of
-    Analyse filepaths -> void $ runExceptT $ Analyser.analyse filepaths
+    Analyse filepaths filterGte filterLt ->
+      void $ runExceptT $ A.analyseTotals filepaths $ toList filterGte A.txnFilterGteCents <> toList filterLt A.txnFilterLtCents
+      where toList m f =
+              case m of
+                Nothing -> []
+                Just d  -> [f (d * 100)]
     Dummy buildId -> putStrLn $ show globalOption <> " : " <> buildId
 
 parseOptions :: Parser Options
 parseOptions =
   Options <$>
-  optional
-    (strOption
-       (short 'g' <> long "global-option" <> metavar "GLOBALOPTION" <> help "Some option global to all commands")) <*>
+  optional (strOption (short 'g' <> long "global-option" <> metavar "GLOBALOPTION" <> help "Some option global to all commands")) <*>
   subparser
     (command
        "analyse"
-       ((Analyse <$> some (argument str (metavar "SOURCE-FILEPATH"))) ? "Analyse statements in specified files") <>
+       ((Analyse <$> some (argument str (metavar "SOURCE-FILEPATH")) <*>
+         optional (option auto (short 'a' <> long "filter-gte" <> metavar "DOLLARS" <> help "Filter txns >= specified dollar amount")) <*>
+         optional (option auto (short 'b' <> long "filter-lt" <> metavar "DOLLARS" <> help "Filter txns < specified dollar amount"))) ?
+        "Analyse statements in specified files") <>
      command "dummy" ((Dummy <$> argument str (metavar "BUILD-ID")) ? "Dummy command taking a build id"))
 
 (?) :: Parser a -> String -> ParserInfo a
--- src/Analyser.hs
+++ src/Analyser.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Analyser
-  ( analyse
+  ( analyseTotals
+  , TxnFilter
+  , txnFilterNone
+  , txnFilterGteCents
+  , txnFilterLtCents
   ) where
 
 import           Control.Monad                 (join)
@@ -11,35 +15,43 @@ import           Data.Bifunctor                (bimap, first)
 import qualified Data.ByteString.Lazy          as BL
 import           Data.Csv                      ((.:))
 import qualified Data.Csv                      as Csv
-import           Data.Foldable                 (toList)
-import           Data.Functor                  (void)
+import           Data.Foldable                 (toList, traverse_)
 import           Data.List                     (sortOn)
 import qualified Data.List.Utils               as U (uniq)
+import           Data.Map                      (assocs)
 import qualified Data.Map                      as M
-import           Data.Text                     hiding (groupBy)
+import           Data.Monoid
+import           Data.Text                     hiding (all, filter, groupBy)
 import qualified Data.Vector                   as V
 import           Text.Parsec                   (ParseError, (<|>))
 import           Text.Parsec.Char              as PC
 import qualified Text.ParserCombinators.Parsec as P
 
-{-
-  Specification
-    . read specified csv files, eg ~/Dropbox/LPSM/personal/visa/20190701.csv
-    . dedup csv lines
-    . parse: Effective Date,Entered Date,Transaction Description,Amount,Balance
-    . sort by txn date
-    . group by transaction description
-    . compare with whitelist
-    . show remaining groups
--}
-analyse :: [FilePath] -> ExceptT AnalyserError IO ()
-analyse filepaths =
-  void $ do
-    files <- traverse parseCsvFile filepaths -- traverse gives IO [[Entry]]
-    let entries = sortOn cEnteredDate $ U.uniq $ join files -- flatten csv lines from multiple files, dedup, sort
-    es <- traverse parseDescription entries
-    let byVendor = groupBy $ keyVendor <$> es
-    liftIO $ print (M.keys byVendor)
+data TxnFilter
+  = TxnFilterNone
+  | TxnFilterGteCents Integer
+  | TxnFilterLtCents Integer
+
+txnFilterNone :: TxnFilter
+txnFilterNone = TxnFilterNone
+
+txnFilterGteCents :: Integer -> TxnFilter
+txnFilterGteCents = TxnFilterGteCents
+
+txnFilterLtCents :: Integer -> TxnFilter
+txnFilterLtCents = TxnFilterLtCents
+
+analyseTotals :: [FilePath] -> [TxnFilter] -> ExceptT AnalyserError IO ()
+analyseTotals filepaths txnFilter = do
+  files <- traverse parseCsvFile filepaths
+  let entries = sortOn cEnteredDate $ U.uniq $ join files
+  es <- traverse parseDescription entries
+  let byVendor = groupBy $ keyByVendor <$> es
+  --let vendors = M.keys byVendor
+  let totalsByVendor = foldMap (\e -> (Sum 1, Sum (cents $ -(eAmount e)))) <$> byVendor
+  let entrySet = assocs totalsByVendor
+  let filtered = filter (passesTxnFilters txnFilter . snd) entrySet
+  liftIO $ traverse_ putStrLn $ formatTotal <$> filtered
 
 parseDescription :: CsvEntry -> ExceptT AnalyserError IO Entry
 parseDescription v = do
@@ -47,13 +59,26 @@ parseDescription v = do
   return $ Entry (cEffectiveDate v) (cEnteredDate v) detail (cAmount v) (cBalance v)
 
 groupBy :: Ord k => [(k, a)] -> M.Map k [a]
-groupBy kvs = M.fromListWith (++) [(k, [v]) | (k, v) <- kvs]
+groupBy kvs = M.fromListWith (<>) [(k, [v]) | (k, v) <- kvs]
 
-keyVendor :: Entry -> (Text, Entry)
-keyVendor e =
+keyByVendor :: Entry -> (Text, Entry)
+keyByVendor e =
   case e of
     Entry _ _ (Txn _ vendor _ _) _ _ -> (vendor, e)
     Entry _ _ (General _) _ _        -> ("(none)", e)
+
+type Agg = (Sum Integer, Sum Integer)
+
+passesTxnFilters :: [TxnFilter] -> Agg -> Bool
+passesTxnFilters fs a = all (passesTxnFilter a) fs
+
+passesTxnFilter :: Agg -> TxnFilter -> Bool
+passesTxnFilter _ TxnFilterNone           = True
+passesTxnFilter a (TxnFilterGteCents c) = getSum (snd a) >= c
+passesTxnFilter a (TxnFilterLtCents c) = getSum (snd a) < c
+
+formatTotal :: (Text, Agg) -> String
+formatTotal (v, (c, t)) = unpack v <> ": " <> show (getSum c) <> " @ $" <> show (dollars $ getSum t)
 
 ----
 -- Effective Date,Entered Date,Transaction Description,Amount,Balance
@@ -95,6 +120,12 @@ data Entry =
     }
   deriving (Show, Eq)
 
+cents :: Double -> Integer
+cents = round . (* 100.0)
+
+dollars :: Integer -> Double
+dollars = (/ 100.0) . fromIntegral
+
 data AnalyserError
   = ParseCsvError String
   | ParseDescriptionError ParseError
@@ -130,10 +161,11 @@ txnParser = do
       (case refundInd of
          Just _  -> Refund
          Nothing -> Purchase)
-      (strip (pack vendor))
+      (strip $ pack vendor)
       (pack details)
       (pack refno)
 
+-- "Cash Advances Interest: $1.98"
 generalParser :: P.Parser Detail
 generalParser = do
   details <- P.many anyChar
