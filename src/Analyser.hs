8fe5a06c96fd6d689445765e5613d6caf0a2c9d8
leigh-perry <lperry.breakpoint@gmail.com>
2019-10-10 06:50:10 +0000

    Regex

--- src/Analyser.hs
+++ src/Analyser.hs
@@ -8,24 +8,26 @@ module Analyser
   , txnFilterLtCents
   ) where
 
-import Control.Monad (join)
-import Control.Monad.IO.Class (liftIO)
-import Control.Monad.Trans.Except (ExceptT, except)
-import Data.Bifunctor (bimap, first)
-import qualified Data.ByteString.Lazy as BL
-import Data.Csv ((.:))
-import qualified Data.Csv as Csv
-import Data.Foldable (toList, traverse_)
-import Data.List (sortOn)
-import qualified Data.List.Utils as U (uniq)
-import Data.Map (assocs)
-import qualified Data.Map as M
-import Data.Monoid
-import Data.Text hiding (all, any, filter, groupBy)
-import qualified Data.Vector as V
-import Text.Parsec (ParseError, (<|>))
-import Text.Parsec.Char as PC
+import           Control.Monad                 (join)
+import           Control.Monad.IO.Class        (liftIO)
+import           Control.Monad.Trans.Except    (ExceptT, except)
+import           Data.Bifunctor                (bimap, first)
+import qualified Data.ByteString.Lazy          as BL
+import           Data.Csv                      ((.:))
+import qualified Data.Csv                      as Csv
+import           Data.Foldable                 (toList, traverse_)
+import           Data.List                     (sortOn)
+import qualified Data.List.Utils               as U (uniq)
+import           Data.Map                      (assocs)
+import qualified Data.Map                      as M
+import           Data.Monoid
+import           Data.Text                     hiding (all, any, filter,
+                                                groupBy)
+import qualified Data.Vector                   as V
+import           Text.Parsec                   (ParseError, (<|>))
+import           Text.Parsec.Char              as PC
 import qualified Text.ParserCombinators.Parsec as P
+import           Text.Regex.Posix              ((=~))
 
 data TxnFilter
   = TxnFilterNone
@@ -67,7 +69,7 @@ filterWhitelist wls =
   filter
     (\e ->
        case eDetail e of
-         Txn _ v _ _ -> not (any (\w -> wVendorRegex w == v) wls)
+         Txn _ v _ _ -> not (any (\w -> unpack v =~ unpack (wVendorRegex w) :: Bool) wls)
          General _ -> True)
 
 groupBy :: Ord k => [(k, a)] -> M.Map k [a]
@@ -77,7 +79,7 @@ keyByVendor :: Entry -> (Text, Entry)
 keyByVendor e =
   case e of
     Entry _ _ (Txn _ vendor _ _) _ _ -> (vendor, e)
-    Entry _ _ (General _) _ _ -> ("(none)", e)
+    Entry _ _ (General _) _ _        -> ("(none)", e)
 
 type Agg = (Sum Integer, Sum Integer)
 
@@ -85,9 +87,9 @@ passesTxnFilters :: [TxnFilter] -> Agg -> Bool
 passesTxnFilters fs a = all (passesTxnFilter a) fs
 
 passesTxnFilter :: Agg -> TxnFilter -> Bool
-passesTxnFilter _ TxnFilterNone = True
+passesTxnFilter _ TxnFilterNone         = True
 passesTxnFilter a (TxnFilterGteCents c) = getSum (snd a) >= c
-passesTxnFilter a (TxnFilterLtCents c) = getSum (snd a) < c
+passesTxnFilter a (TxnFilterLtCents c)  = getSum (snd a) < c
 
 formatTotal :: (Text, Agg) -> String
 formatTotal (v, (c, t)) = unpack v <> ": " <> show (getSum c) <> " @ $" <> show (dollars $ getSum t)
@@ -98,10 +100,10 @@ formatTotal (v, (c, t)) = unpack v <> ": " <> show (getSum c) <> " @ $" <> show 
 data CsvEntry =
   CsvEntry
     { cEffectiveDate :: !Text
-    , cEnteredDate :: !Text
-    , cDescription :: !Text
-    , cAmount :: !Double
-    , cBalance :: !Double
+    , cEnteredDate   :: !Text
+    , cDescription   :: !Text
+    , cAmount        :: !Double
+    , cBalance       :: !Double
     }
   deriving (Show, Eq)
 
@@ -115,27 +117,27 @@ data Detail
       { gDescription :: !Text
       }
   | Txn
-      { eType :: !TxnType
-      , eVendor :: !Text
+      { eType    :: !TxnType
+      , eVendor  :: !Text
       , eDetails :: !Text
-      , eRef :: !Text
+      , eRef     :: !Text
       }
   deriving (Show, Eq)
 
 data Entry =
   Entry
     { eEffectiveDate :: !Text
-    , eEnteredDate :: !Text
-    , eDetail :: !Detail
-    , eAmount :: !Double
-    , eBalance :: !Double
+    , eEnteredDate   :: !Text
+    , eDetail        :: !Detail
+    , eAmount        :: !Double
+    , eBalance       :: !Double
     }
   deriving (Show, Eq)
 
 data WhitelistEntry =
   WhitelistEntry
     { wVendorRegex :: !Text
-    , wTxnLimit :: !Double
+    , wTxnLimit    :: !Double
     }
   deriving (Show, Eq)
 
@@ -188,7 +190,7 @@ txnParser = do
   return $
     Txn
       (case refundInd of
-         Just _ -> Refund
+         Just _  -> Refund
          Nothing -> Purchase)
       (strip $ pack vendor)
       (pack details)
