69c36ca9aafc49157fda00a27c3fae56fdd30fbf
s117476 <Leigh.Perry@iag.com.au>
2019-10-10 14:53:44 +1100

    Basic filtering

--- runit.sh
+++ runit.sh
@@ -1,3 +1,7 @@
-./.stack-work/install/x86_64-osx/ea1da75a17412f2c0d18b3add3fe2465d49124cde5a48bb7201c7ff4bcebab93/8.6.5/bin/bankcheck analyse ~/Dropbox/LPSM/personal/visa/20190701.csv
-#./.stack-work/install/x86_64-osx/ea1da75a17412f2c0d18b3add3fe2465d49124cde5a48bb7201c7ff4bcebab93/8.6.5/bin/bankcheck analyse ~/Dropbox/LPSM/personal/visa/20190401.csv ~/Dropbox/LPSM/personal/visa/20190701.csv
-#./.stack-work/install/x86_64-osx/ea1da75a17412f2c0d18b3add3fe2465d49124cde5a48bb7201c7ff4bcebab93/8.6.5/bin/bankcheck analyse ~/Dropbox/LPSM/personal/visa/*.csv
+#!/bin/bash
+
+EXEPATH=.stack-work/install/x86_64-osx/4b09fd05ca2f71b9cf969b6906a96ebb0ce44fd665d51dbd56cd05f7400f42d9/8.6.5/bin
+#EXEPATH=.stack-work/install/x86_64-osx/ea1da75a17412f2c0d18b3add3fe2465d49124cde5a48bb7201c7ff4bcebab93/8.6.5/bin/
+${EXEPATH}/bankcheck analyse ~/Dropbox/LPSM/personal/visa/20190701.csv "$@"
+#${EXEPATH}/bankcheck analyse ~/Dropbox/LPSM/personal/visa/20190401.csv ~/Dropbox/LPSM/personal/visa/20190701.csv "$@"
+#${EXEPATH}/bankcheck analyse ~/Dropbox/LPSM/personal/visa/*.csv "$@"
